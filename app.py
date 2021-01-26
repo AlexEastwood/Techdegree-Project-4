81309c23974919f131e71f92edb58884cb8629e7
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-01-26 13:49:31 +0000

    Initialized db and added csv data

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from peewee import *
 db = SqliteDatabase("inventory.db")
 
 class Entry(Model):
-    product_id = CharField(primary_key=True)
-    product_name = CharField()
+    product_id = AutoField()
+    product_name = CharField(unique=True)
     product_quantity = IntegerField()
     product_price = IntegerField()
     date_updated = DateField(default=datetime.datetime.now)
@@ -18,22 +18,36 @@ class Entry(Model):
     class Meta:
         database = db
         
+def add_items():     
+    with open("inventory.csv", newline = "") as csvfile:
+        invreader = csv.DictReader(csvfile, delimiter = ",")
+        items = list(invreader)
+        for item in items:
+            item["product_quantity"] = int(item["product_quantity"])
+            item["product_price"] = int(float(item["product_price"][1:]) * 100)
+            item["date_updated"] = (datetime.datetime.strptime(item['date_updated'],'%m/%d/%Y').date())
+            try:
+                Entry.create(
+                product_name = item['product_name'],
+                product_price = item['product_price'],
+                product_quantity = item['product_quantity'],
+                date_updated = item['date_updated'],
+                ).save()
+            except IntegrityError:
+                item_update = Entry.get(product_name = item['product_name'])
+                item_update.product_name = item['product_name']
+                item_update.product_price = item['product_price']
+                item_update.product_quantity = item['product_quantity']
+                item_update.date_updated = item['date_updated']
+                item_update.save()
+                
 def initialize():
     db.connect()
     db.create_tables([Entry], safe=True)
-    
+  
 if __name__ =="__main__":
     initialize()
-    
-    with open("inventory.csv", newline = "") as csvfile:
-        invreader = csv.DictReader(csvfile, delimiter = ",")
-        rows = list(invreader)
-        for row in rows[1:]:
-            row["product_quantity"] = int(row["product_quantity"])
-            row["product_price"] = int(float(row["product_price"][1:]) * 100)
-            row["date_updated"] = datetime.datetime.strptime(row["date_updated"], "%m/%d/%Y")
-        
-        print(rows)
+    add_items()
             
             
             
